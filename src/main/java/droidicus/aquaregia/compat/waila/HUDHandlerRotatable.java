f3b9898412d0c345681e6bcccdf337d3d4e75179
droidicus <droidicus@dr.com>
2016-09-07 00:20:43 -0400

    Disable WAILA compat for now to fix language file leaks

--- src/main/java/droidicus/aquaregia/compat/waila/HUDHandlerRotatable.java
+++ src/main/java/droidicus/aquaregia/compat/waila/HUDHandlerRotatable.java
@@ -1,18 +1,18 @@
-package droidicus.aquaregia.compat.waila;
-
-import net.minecraft.block.properties.IProperty;
-import net.minecraft.util.EnumFacing;
-
-/**
- * @author Choonster
- */
-public class HUDHandlerRotatable extends HUDHandlerEnumProperty<EnumFacing> {
-
-	public HUDHandlerRotatable(IProperty<EnumFacing> property) {
-		this(property, "aquaregia:rotatable.facing.desc");
-	}
-
-	public HUDHandlerRotatable(IProperty<EnumFacing> property, String tooltipTranslationKey) {
-		super(property, tooltipTranslationKey, "aquaregia:facing");
-	}
-}
+//package droidicus.aquaregia.compat.waila;
+//
+//import net.minecraft.block.properties.IProperty;
+//import net.minecraft.util.EnumFacing;
+//
+///**
+// * @author Choonster
+// */
+//public class HUDHandlerRotatable extends HUDHandlerEnumProperty<EnumFacing> {
+//
+//	public HUDHandlerRotatable(IProperty<EnumFacing> property) {
+//		this(property, "aquaregia:rotatable.facing.desc");
+//	}
+//
+//	public HUDHandlerRotatable(IProperty<EnumFacing> property, String tooltipTranslationKey) {
+//		super(property, tooltipTranslationKey, "aquaregia:facing");
+//	}
+//}
